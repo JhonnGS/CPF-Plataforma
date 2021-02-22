997532acac6cb7ceb6bf9d9f3962874a81d3f4ce
GOSJ GOMSO <GOMSO_07@outlook.com>
2021-02-22 13:42:46 -0600

    Agregar archivos de proyecto.

---
+++ CPF-Plataforma/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CPF_Plataforma
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
